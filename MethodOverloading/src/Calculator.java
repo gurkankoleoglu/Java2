3a11a7561446fdce3f2564f1edb7374a302b040b
hp <hp@LAPTOP-E4ECGHHP>
2022-09-24 08:54:33 +0300

    Second Java Commit

---
+++ MethodOverloading/src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+	
+	public int sum(int num1, int num2) {
+		return num1 + num2;
+	}
+	
+	public int sum(int num1, int num2, int num3) {
+		return num1 + num2 + num3;
+	}
+}
---
+++ MethodOverloading/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Calculator calculator = new Calculator();
+		System.out.println(calculator.sum(4, 9));
+		System.out.println(calculator.sum(4, 9, 7));
+	}
+}
